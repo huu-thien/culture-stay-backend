8c63d822a2618f35ade7836f7ecedee920c00e76
huu-thien <thiencute180402@gmail.com>
2024-06-02 00:08:48 +0700

    [BE] Add backgound job remove booking exprixed

---
+++ CultureStay.Application/BackgroundServices/RemovePendingBookingJob.cs
@@ -0,0 +1,26 @@
+﻿using CultureStay.Application.ViewModels.Booking.Specifications;
+using CultureStay.Domain.Entities;
+using CultureStay.Domain.Repositories.Base;
+using Quartz;
+
+namespace CultureStay.Application.BackgroundServices;
+
+public class RemovePendingBookingJob : IJob
+{
+    private readonly IRepositoryBase<Booking> _bookingRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public RemovePendingBookingJob(IRepositoryBase<Booking> bookingRepository, IUnitOfWork unitOfWork)
+    {
+        _bookingRepository = bookingRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        var bookings = await _bookingRepository.FindListAsync(new ExpiredBookingSpecification());
+        _bookingRepository.DeleteRange(bookings);
+        await _unitOfWork.SaveChangesAsync();
+    }
+}
---
+++ CultureStay.Application/ViewModels/Booking/Specifications/ExpiredBookingSpecification.cs
@@ -0,0 +1,13 @@
+﻿using CultureStay.Domain.Enum;
+using CultureStay.Domain.Specification;
+
+namespace CultureStay.Application.ViewModels.Booking.Specifications;
+
+public class ExpiredBookingSpecification : Specification<Domain.Entities.Booking>
+{
+    public ExpiredBookingSpecification()
+    {
+        AddFilter(p => p.Status == BookingStatus.Pending);
+        AddFilter(p => p.CheckInDate < DateTime.Now.AddDays(-2));
+    }
+}
